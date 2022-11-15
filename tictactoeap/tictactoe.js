b1c509b948939b40ec4413f253732b0322d8b425
Comfy <kenwadder27@gmail.com>
2022-11-15 17:57:51 -0600

    added html board buttons, winner overlay, now working on js logic

--- tictactoeap/tictactoe.js
+++ tictactoeap/tictactoe.js
@@ -3,5 +3,18 @@ const gameBoard = (() => {
 
     const getSpot = (num) => boardSpot[num];
 
-    
+    const setSpot = (num, player) => {
+        const htmlSpot = document.querySelector('#gameBoardContainer:nth-child(${num + 1})p');
+
+    }
+
 })();
+
+const Player = (type) => {
+    this.type= type;
+
+    const getType = () => {
+        return type;
+    }
+    return {getType};
+};
