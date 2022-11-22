6057dc3389ac1ad8213d30e98a115f16d9a4fdd2
Comfy <kenwadder27@gmail.com>
2022-11-22 17:26:22 -0600

    added more logic to the control of the game, need to finish the display and test the game logic

--- tictactoeap/tictactoe.js
+++ tictactoeap/tictactoe.js
@@ -105,8 +105,107 @@ const gameController = (() => {
         }
         else throw 'Incorrect type';
     }
-
-    return {
-        changeType
+    const checkRow = (board) => {
+        for(let i=0; i < 3; i++) {
+            let row= []
+            for(let j=i*3;j<i*3+3; j++){
+                row.push(board.getSpot(j));
+            }
+            if(row.every(spot => spot =='X')|| row.every(spot=> spot== 'O')){
+                return true;
+            }
+        }
+        return false;
     }
+    const checkColumn = (board) => {
+        for(let i=0;i<3;i++){
+            let column=[]
+            for(let j= 0;j<3;j++){
+                column.push(board.getSpot(i+3*j));
+            }
+            if(column.every(spot => spot=='X')|| column.every(spot=> spot=='O')){
+                return true;
+            }
+        }
+        return false;
+    }
+    const checkDiagonals =(board)=> {
+        diagonal1 =[board.getSpot(0),board.getSpot(4), board.getSpot(8)];
+        diagonal2 =[board.getSpot(6),board.getSpot(4), board.getSpot(2)];
+        if(diagonal1.every(spot=> spot == 'X')||diagonal2.every(spot=> spot =='O')){
+            return true;
+        }
+        else if(diagonal2.every(spot => spot =='X') || diagonal2.every(spot => spot == 'O')){
+            return true;
+        }
+    }
+    const checkWin = (board) => {
+        if(checkRow(board)||checkColumn(board)||checkDiagonals(board)){
+            return true;
+        }
+        return false;
+    }
+    const checkDraw = (board) => {
+        if(checkWin(board)){
+            return false;
+        }
+        for(let i=0; i<9;i++){
+            const spot= board.getSpot(i);
+            if(spot == undefined){
+                return false;
+            }
+        }
+        return true;
+    }
+    const endGame = function(type) {
+        const card =document.getElementById('card');
+        card.classList.remove('unblur');
+        card.classList.add('blur');
+
+        playerWin= document.getElementById('player');
+        computerWin= document.getElementById('computer');
+        draw=document.getElementById('draw');
+
+        if (type=='Draw'){
+            draw.classList.remove('hide');
+            console.log('draw');
+        }
+        else {
+            if(type.toLowerCase() == player.getType()){
+                playerWin.classList.remove('hide');
+            }
+            else{
+                computerWin.classList.remove('high');
+            }
+        }
+
+    }
+    const playerStep = (num) => {
+        const spot =gameBoard.getSpot(num);
+        if(spot == undefined) {
+            gameBoard.setSpot(num,player);
+            if(checkWin(gameBoard)) {
+                endGame(player.getType());
+            }
+            else if (checkDraw(gameBoard)){
+            endGame("Draw");
+            }
+            else {
+                displayController
+            }
+        }
+        else{
+        console.log('already filled!!');
+        }
+    }
+    return {
+        changeType,
+        getPlayer,
+        getComputer,
+        checkWin,
+        checkDraw,
+        playerStep,
+        endGame
+    }
+
 })();
