8d6d56e5c4b38ee86b685162ea3f1e10ebb69cbe
Comfy <kenwadder27@gmail.com>
2023-01-04 12:50:27 -0600

    set up debwebserver, index.html is set up, creating element in index.js

---
+++ restaurantproject/src/index.js
@@ -0,0 +1,9 @@
+console.log('hello')
+
+
+function display() {
+    const header = document.createElement('div');
+    const headertab = document.createElement('div');
+    const content= document.getElementById('content');
+}
+
